a3a8feb0fdb6e40b5ccee397e4f0172649be82e6
Benjamin Summers <bsummer4@eecs.utk.edu>
2014-03-13 21:00:33 +0000

    First attempt and handling HTTP Basic Authentication.

--- Server.hs
+++ Server.hs
@@ -1,18 +1,13 @@
 -- TODO Add an authentication endpoint, and support token-based authentication.
--- TODO Add support for HTTP Basic Authentication.
---     TODO Are there any libraries to handle this more easily?
---     TODO Read about padding schemes.
---     TODO Define sensible limits on username and password length.
---         TODO Enforce them.
---     TODO Come up with a sane way to handle passing in encryption keys at
---         run time or compile time.
--- TODO Better Response Codes
---     200 OK, 201 Created, 204 No Content
---     400 Bad Request, 404 Not Found, 403 Forbidden, 401 Unauthorized
+-- TODO Get encryption keys from environment variables.
+-- TODO Propefly hash the passwords, and don't store them as plaintext.
+-- TODO Define and enforce sensible restrictions on username and password.
+--     Usernames in HTTP cannot contain a colon.
+--     Length restrictions?
 
 {-# LANGUAGE OverloadedStrings, UnicodeSyntax, QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell, DeriveDataTypeable, TypeFamilies #-}
-{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE NoImplicitPrelude, ScopedTypeVariables #-}
 
 import ClassyPrelude
 import Prelude.Unicode
@@ -26,10 +21,65 @@ import qualified Data.Aeson as J
 import qualified Web.ClientSession as Session
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
+import qualified Data.Text as T
 import Data.Acid
 import Control.Monad.State (put)
 import Control.Monad.Reader (ask)
 import Data.SafeCopy
+import qualified Data.ByteString.Base64 as Base64
+
+
+-- [[Authentication Stuff]]
+data AuthParse = Anon | Malformed | AuthAttempt{realm∷Text,uname∷Text,pass∷Text}
+type Registrations = Map User Password
+
+userPassFromBasicAuthString ∷ Text → Maybe (Text,Text)
+userPassFromBasicAuthString s = case Base64.decode $ encodeUtf8 s of
+    Left _ → Nothing
+    Right pair → case T.breakOn ":" $ decodeUtf8 pair of
+        (_,"") → Nothing
+        (user,colonPass) → Just (user, T.tail colonPass)
+
+unPrefix ∷ Text → Text → Maybe Text
+unPrefix prefix str = case T.splitAt (length prefix) str of
+    (strHead, strTail) → if strHead≠prefix then Nothing else Just strTail
+
+parseAuthorization ∷ ByteString → Maybe (Text,Text)
+parseAuthorization =
+    join∘fmap userPassFromBasicAuthString∘unPrefix "Basic "∘decodeUtf8
+
+parseWWWAuthenticate ∷ ByteString → Maybe Text
+parseWWWAuthenticate s = case unPrefix "Basic realm=\"" $ decodeUtf8 s of
+    Nothing → Nothing
+    Just "" → Nothing
+    Just(shit∷Text) → case T.splitAt (length shit-1) shit of
+        (r,"\"") → Just r
+        _ → Nothing
+
+authAttempt ∷ W.Request → AuthParse
+authAttempt req = f where
+    hdrs = W.requestHeaders req
+    parse a b = case (parseWWWAuthenticate a, parseAuthorization b) of
+        (Just aRealm, Just(u,p)) → AuthAttempt aRealm u p
+        _ → Malformed
+    f = case (lookup "WWW-Authenticate" hdrs, lookup "Authorization" hdrs) of
+        (Nothing, Nothing) → Anon
+        (Just a, Just b) → parse a b
+        _ → Malformed
+
+authFailed ∷ IO W.Response
+authFailed = return $ W.responseLBS W.status403 [] ""  -- TODO Is this right?
+
+-- This rejects requests with invalid credentials. Any requests that pass
+-- through this filter will either have no credentials or correct credentials.
+basicAuth ∷ Registrations → AcidState Data → W.Application → W.Application
+basicAuth users _ waiapp req = case authAttempt req of
+    Anon → waiapp req
+    Malformed → authFailed
+    AuthAttempt _ u p → case M.lookup u users of
+        Nothing → authFailed
+        Just(password) → if password≡p then waiapp req else
+            return $ W.responseLBS W.status400 [] ""
 
 
 -- [[Misc]]
@@ -88,12 +138,9 @@ getData db = query db QueryData
 
 
 -- [[Application Logic]]
-data Resp = NotOk | Ok | URLs (Set URL) | ServeClient
-    deriving Show
-
+data Resp = NotOk | Ok | URLs (Set URL) | ServeClient deriving Show
 data Req
-    = BadReq
-    | WebClient
+    = BadReq | WebClient
     | AddURL User URL | DelURL User URL | GetURLs User | SetURLs User (Set URL)
     deriving Show
 
@@ -147,6 +194,6 @@ app db webreq = do
 main ∷ IO()
 main = do
     db ← openLocalState (Data M.empty)
-    finally (W.run 8080 $ app db) $ do
+    finally (W.run 8080 $ basicAuth M.empty db $ app db) $ do
         createCheckpoint db
         closeAcidState db
