0f462c7803713a2745dee4ade31d450f814473cb
Benjamin Summers <bsummer4@eecs.utk.edu>
2014-03-21 14:36:57 +0000

    Fixed some bugs in the deploy script. It works!

--- deploy.sh
+++ deploy.sh
@@ -3,33 +3,32 @@
 # that we are uploading to another server running the same distribution.
 
 set -e
-
 uploadto=$1
-deploy=$(mktemp -dp.)
-deploy=$(realpath $deploy)
-
-trap "rm -rf $deploy" EXIT
-
-test -z "$deploy" || {
+echo $uploadto
+test -n "$deploy" || {
     echo -n "Where should we scp the files? ";
     read uploadto; }
 
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install haskell-platform git
+sudo apt-get install haskell-platform git realpath
+
 cabal update
 cabal install cabal-install
 export PATH="$HOME/.cabal/bin:$PATH"
 
 git clone https://github.com/bsummer4/habits.git
 cd habits
-git pull
 cabal sandbox init
 cabal install --only-dependencies
 cabal build --ghc-options='-O3 -Wall -static'
 
+deploy=$(mktemp -dp.)
+deploy=$(realpath $deploy)
+trap "rm -rf $deploy" EXIT
+
 cp dist/build/habits/habits $deploy/habits
 cd $deploy
-openssl aes-256-cbc -d <../secret.tar.xz.aes | unxz | tar x)
+openssl aes-256-cbc -d <../secret.tar.xz.aes | unxz | tar x
 cp ../index.html .
 scp * $uploadto
