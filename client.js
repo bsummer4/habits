bf5a4e57adc7aac493554252d99f069c1c1297f8
Benjamin Summers <bsummer4@eecs.utk.edu>
2014-05-13 23:29:55 +0000

    Everything is now done with react components, but interactivity doesn't work yet.

--- client.js
+++ client.js
@@ -2,12 +2,25 @@
  * @jsx React.DOM
  */
 
+//////////////////////////////////////////////////////////////////////////////
+// App State
+//
+// data Status = "success" | "failure" | "unspecified"
+// type Habit = String
+// type Token = String
+// type User = String
+// type Day = Int // Modified Julian Day
+// type Adherence = Day → Habit →
+//   (status:Status, num:Maybe Number, chains:Number)
+// type State = {day:Day user:User, tok:Token, days:Adherence}
+
 
 //////////////////////////////////////////////////////////////////////////////
 // Pure Functions
-Date.prototype.modifiedJulianDay = function() {
+Date.prototype.modifiedJulianDay = function(){
   return Math.floor((this/86400000)-(this.getTimezoneOffset()/1440)+40587) }
 
+var arb = function(d) { return d[_.keys(d)[0]]; }
 var normalizeNote = function(note) { return note.replace(/\s+/g,' ').trim() }
 var julian = function(d) { return d.modifiedJulianDay() }
 var epoch = julian(new Date(0))
@@ -15,18 +28,52 @@ var fromJulian = function(j) { return new Date(86400000*(j-epoch)) }
 var day = julian(new Date())
 var member = function(i,a) { return !(-1 === a.indexOf(i)) }
 var cssPercent = function(x) { return (x*100) + "%" }
-var concat = function(arrays) { return [].concat.apply([],arrays); }
+var concat = function(arrays) { return [].concat.apply([],arrays) }
+var orWhat = function(n) { (n===null || n.length===0) ? null : n }
+var statusCycle = function (st) {
+  if (st == "success") return "failure"
+  if (st == "failure") return "unspecified"
+  if (st == "unspecified") return "success"
+  return null }
+
+var habitNum = function(habit){
+  var x = null
+  if ("Success" in habit) { x=habit["Success"] }
+  if ("Failure" in habit) { x=habit["Failure"] }
+  if ("Unspecified" in habit) { x=habit["Unspecified"] }
+  if ("number" != typeof x) { x=null }
+  // console.log(habit,x)
+  return x }
+
+var habitClass = function(habit){
+  if ("Success" in habit) { return "success" }
+  if ("Failure" in habit) { return "failure" }
+  if ("Unspecified" in habit) { return "unspecified" }
+  return null }
+
+
+//////////////////////////////////////////////////////////////////////////////
+// Utility Procedures
+var strSortInPlace = function (x) {
+  x.sort(function(a,b){
+    var t1 = "".concat(a)
+    t1.toUpperCase()
+    var t2 = "".concat(b)
+    t2.toUpperCase()
+    return t1.localeCompare(t2) }) }
+
+var fsort = function(s) { strSortInPlace(s); return s }
 
 
 //////////////////////////////////////////////////////////////////////////////
 // React Components
 var LoginForm = React.createClass({
-  handleSubmit: function() {
+  handleSubmit: function(){
     user = this.refs.user.getDOMNode().value.trim()
     var pass = this.refs.pass.getDOMNode().value.trim()
     if (user && pass) this.props.login(user,pass)
     return false; },
-  render: function() {
+  render: function(){
     return (<form className="loginForm" onSubmit={this.handleSubmit}>
       <input type="text" ref="user" placeholder="Username"></input>
       <input type="Password" ref="pass" placeholder="Password"></input>
@@ -34,32 +81,32 @@ var LoginForm = React.createClass({
       </form> )} })
 
 var LogoutForm = React.createClass({
-  handleSubmit: function () {
+  handleSubmit: function (){
     this.props.logout(); return false; },
-  render: function() {
+  render: function(){
     return (<form className="logoutForm" onSubmit={this.handleSubmit}>
       <input type="submit" value="Logout" />
       </form> )}})
 
 var AddNoteForm = React.createClass({
-  handleSubmit: function () {
+  handleSubmit: function (){
     var note = this.refs.name.getDOMNode().value.trim();
     if (note === "") return false;
     this.props.addNote(note);
     return false; },
-  render: function() {
+  render: function(){
     return (<form onSubmit={this.handleSubmit}>
       <input placeholder="Add a Note" ref="name" className="note" type="text" />
       </form>) }})
 
 var Note = React.createClass({
-  submit: function() {
+  submit: function(){
     var fresh = normalizeNote(this.refs.name.getDOMNode().value)
     var old = normalizeNote(this.props.name)
     if (fresh == old) { return false }
     this.props.renameNote(old,fresh);
     return false; },
-  render: function() { return (
+  render: function(){ return (
     <form onSubmit={this.submit}>
       <input ref="name" className="note" defaultValue={this.props.name} />
       </form> )}})
@@ -69,7 +116,7 @@ var Notes = React.createClass({
     <li className="note">
       <Note name={note} renameNote={this.props.renameNote} />
       </li> )},
-  render: function() { return(
+  render: function(){ return(
     <p><ul className="note">
       <li className="note" style={{"border-width":2}}>
         <AddNoteForm addNote={this.props.addNote} /></li>
@@ -78,7 +125,8 @@ var Notes = React.createClass({
 
 // <HistoryRect x y status day habit>
 var HistoryRect = React.createClass({
-  render: function() {
+  render: function(){
+    // console.log("rect", this.props)
     var x = 10*this.props.x;
     var y = 10*this.props.y;
     var cx = ['refutable', this.props.status].join(" ")
@@ -86,44 +134,112 @@ var HistoryRect = React.createClass({
 
 // The ‘padding-bottom’ css is a hack to set the height relative to the width.
 var History = React.createClass({
-  render: function() {
+  render: function(){
     var d = this.props.habitData
-    if (0 === d.length) { return <div /> }
-    width = 10 * d[0].length;
-    height = 10 * d.length;
+    var days = fsort(_.keys(d))
+      if (0 === days.length) { return <div /> }
+    var habits = fsort(_.keys(d[days[0]]))
+      if (0 === habits.length) { return <div /> }
+    var width = 10 * days.length;
+    var height = 10 * habits.length;
     var viewbox = [0,0,width,height].join(" ")
     var relHeight = cssPercent(height/width)
+
     return <div style={{width:"100%", height:0, "padding-bottom":relHeight}}>
       <svg width={"100%"} height={"100%"} viewBox={viewbox}>
-        {_.map(d, function(row,j){
-          return _.map(row, function(cell,i){
-            var day=cell[0], habit=cell[1], status=cell[2];
+        {_.map(days, function(day,i){ return(
+          _.map(habits, function(hab,j){
+            var s = d[day][hab].status;
             return <HistoryRect
-              x={i} y={j} status={status} day={day} habit={habit} /> })})}
+              x={i} y={j} status={s} day={day} habit={hab} /> }))})}
         </svg>
       </div> }})
 
+var update = function() { return null; }
 
-//////////////////////////////////////////////////////////////////////////////
-// Global State and Operations on It
-var tok = localStorage.getItem("token")
-var user = localStorage.getItem("username")
-var shiftDate = function(n,cont){ return(function(){ day+=n; cont() })}
-onEnter = function(j,f) {
-  j.on("keyup",function(e){
-    if (e.keyCode == 13)
-      f() })
-  return j }
+var DayNav = React.createClass({
+  dayStr: function (d) { return (
+    " " + d.getFullYear() + "/" + d.getMonth() + "/" + d.getDate() + " ")},
+  back: function(){},
+  forward: function(){},
+  render: function(){
+    return (<h1 className="header">
+      <span className="datechange" onClick={shiftDate(-1,update)}>&lt;&lt;
+        </span>
+      {this.dayStr(fromJulian(this.props.day))}
+      <span className="datechange" onClick={shiftDate(1,update)}>&gt;&gt;
+        </span>
+      </h1> )}})
 
-var habitDOM = function(name, status, tail, callback) {
-  var result = $("<span>")
-  result.addClass("refutable")
-  result.text(name)
-  result.click(callback)
-  if (status) result.addClass(status)
-  if (tail) result.append(tail)
-  return result }
+// <Habit name chain num callback>
+var Habit = React.createClass({render: function(){
+  // console.log("hihi",this.props);
+  cx = ["refutable", this.props.status].join(" ")
+  return(<span>
+    <span style={{"white-space":"nowrap"}}>
+      <span className={"pairLeft "+cx} onClick={this.props.callback}>
+        {this.props.name}
+        {this.props.chain ? [<sup>{this.props.chain}</sup>] : []}
+        </span>
+      {null!==this.props.num ? [<sub className={"pairRight "+cx}>
+                                  {this.props.num}</sub>] : []}
+      </span>
+    &#8203;</span> )}})
 
+var AddHabit = React.createClass({render: function(){
+  return (<input
+    className="refutable" placeholder="addhabit"
+    style={{width:"70px", "font-weight":"bold"}} /> )}})
+
+// habitInfo ∷ habit → {status chain num}
+// <HabitList habitInfo>
+var HabitList = React.createClass({render: function (){
+  var d = this.props.habitInfo // Day → {status num chains}
+  var names = fsort(_.keys(d))
+  var cb = function(){ console.log("clicked!"); }
+  return (<p>
+    {_.map(names, function (nm) { return(
+      <Habit
+        status={d[nm].status}
+        name={nm}
+        chain={d[nm].chains}
+        num={d[nm].num}
+        callback={cb} /> )})}
+    <span> </span>
+    <AddHabit />
+    </p> )}})
+
+var App = React.createClass({
+  getInitialState: function() {
+    var user = localStorage.getItem("username")
+    var tok = localStorage.getItem("token")
+    var day = julian(new Date())
+    var nulls = {day:day, tok:tok, user:user, days:{}}
+    console.log(user,tok,day)
+    var fuck = this;
+    if (user && tok) {
+      getUpdates(user, tok, day, function(x){fuck.setState(x)}) }
+    return nulls; },
+
+  render: function(){
+    var st = this.state
+    if (!(st.user && st.tok)) { return(
+      <div>
+        Please login or register. If you try to login with a username that
+        doesn't exist, the we will register you instead.
+        <LoginForm login={login}/>
+        </div> )}
+    return(
+      <ul>
+        <DayNav day={st.day} />
+        <HabitList habitInfo={st.days[st.day]} />
+        <History habitData={st.days} />
+        <LogoutForm logout={logout} />
+        </ul> )}})
+
+
+///////////////////////////////////////////////////////////////////////////////
+// RPC Calls
 var rpc = function (tok,ty,req,cont) {
   obj = {}
   obj[ty] = [tok,req]
@@ -155,11 +271,11 @@ var getChains = function(cont) {
   rpc(tok,"Query",{GetChains:[user,day]},cont) }
 
 var delNote = function(note,cont) {
-  return function() {
+  return function(){
     rpc(tok,"Update",{DelNote:[user,day,note]},cont) }}
 
 var delHabit = function(habit,cont) {
-  return function() {
+  return function(){
     rpc(tok,"Update",{DelHabit:[user,habit]},cont) }}
 
 var getNotes = function(cont) {
@@ -172,9 +288,48 @@ var habitsStatus = function(habitSet, day, cont) {
   rpc(tok,"Query",{"GetHabitsStatus":[user,day]},function(r){
     cont(r["STATUSES"]) }) }
 
-var update = function() {
+var setDone = function(day, habit, statusCode, num, cont) {
+  var status
+  if (statusCode == "success") { status={"Success":num} }
+  else if (statusCode == "failure") { status={"Failure":num} }
+  else if (statusCode == "unspecified") { status={"Unspecified":[]} }
+  else { throw "bad statusCode" }
+  return (function(){
+    rpc(tok,"Update",{"SetHabitsStatus":[user,day,habit,status]}, cont) }) }
+
+
+//////////////////////////////////////////////////////////////////////////////
+// Global State and Operations on It
+var tok = localStorage.getItem("token")
+var user = localStorage.getItem("username")
+var shiftDate = function(n,cont){ return(function(){ day+=n; cont() })}
+
+var toState = function(habitSet, statuses, notes, chains, history) {
+  console.log("toState", habitSet, statuses, notes, chains, history)
+  var today = day
+  var days = {}
+  _.forEach(history, function(_,day){ days[day]={} })
+  _.forEach(statuses, function(status, habit) {
+    _.forEach(history, function(_, day) {
+      days[day][habit] =
+        { status: habitClass(history[day][habit])
+        , num: habitNum(history[day][habit])
+        , chains: 0 }})})
+  _.forEach(habitSet, function(habit) {
+    if (habit in chains) {
+      console.log("sup", habit,chains)
+      days[today][habit].chains = chains[habit] }})
+  _.forEach(habitSet, function(habit) {
+    _.forEach(history, function(_, day) {
+      if (!(habit in days[day])) {
+        days[day][habit] = {chains:0, status:"unspecified", num:null } }})})
+  console.log("woot!")
+  return {day:day, user:user, tok:tok, days:days} }
+
+var getUpdates = function(user, tok, day, cont){
+  console.log("getUpdates!", user,tok,day);
   getHistory30(function(historyResponse) {
-    var history = historyResponse["HISTORY"];
+    var history = historyResponse["HISTORY"]
     getChains(function(chainsResponse) {
       chains = chainsResponse["CHAINS"]
       getNotes(function(noteResponse) {
@@ -182,114 +337,28 @@ var update = function() {
         allHabits(function(response) {
           var habitSet = response["HABITS"]
           habitsStatus(habitSet, day, function(statuses) {
-            writeDOM(habitSet, statuses, notes, chains, history) }) }) }) }) }) }
+            cont(toState(habitSet,statuses,notes,chains,history)) })})})})})}
 
-var setDone = function(day, habit, statusCode, num, cont) {
-  var status
-  if (statusCode == "success") { status={"Success":num} }
-  else if (statusCode == "failure") { status={"Failure":num} }
-  else if (statusCode == "unspecified") { status={"Unspecified":[]} }
-  else { throw "bad statusCode" }
-  return (function() {
-    rpc(tok,"Update",{"SetHabitsStatus":[user,day,habit,status]}, cont) }) }
+// var writeDOM = function(state) {
+  // console.log("day", state.day)
+  // console.log("user", state.user)
+  // console.log("tok", state.tok)
+  // console.log("habits", state.habits)
+  // console.log("days:", fsort(_.keys(state.days)))
+  // console.log("[days]")
+  // _.forEach(fsort(_.keys(state.days)), function(day) {
+    // console.log(day)
+    // _.forEach(fsort(_.keys(state.days[day])), function(habit) {
+      // console.log("\t",habit,toJSON(state.days[day][habit])) })})
 
-var strSortInPlace = function (x) {
-  x.sort(function(a,b){
-    var t1 = "".concat(a)
-    t1.toUpperCase()
-    var t2 = "".concat(b)
-    t2.toUpperCase()
-    return t1.localeCompare(t2) }) }
-
-var fsort = function(s) { strSortInPlace(s); return s; }
-var orWhat = function(n) {
-  if (n===null || n.length===0) { return null }
-  return n }
-
-var habitClass = function(habit){
-  if ("Success" in habit) return "success";
-  if ("Failure" in habit) return "failure";
-  if ("Unspecified" in habit) return "unspecified";
-  return null; }
-
-var writeDOM = function(habitSet, statuses, notes, chains, history) {
-  var hdr = $("<h1>")
-  hdr.addClass("header")
-  var back = $("<span>&lt&lt</span>")
-  var forward = $("<span>&gt&gt</span>")
-  back.addClass("datechange")
-  forward.addClass("datechange")
-  back.click(shiftDate(-1, update))
-  forward.click(shiftDate(1, update))
-  hdr.append(back)
-  var d = fromJulian(day)
-  hdr.append(" "+d.getFullYear()+"/"+d.getMonth()+"/"+d.getDate()+" ")
-  hdr.append(forward)
-
-  var habitList = $("<p>")
-  var habitNames = _.keys(statuses)
-  strSortInPlace(habitNames)
-  _.forEach(habitNames, function(nm){
-    var status, alt, num=null
-    if ("Success" in statuses[nm])
-      { status="success"; alt="failure"; num=orWhat(statuses[nm]["Success"]) }
-    else if ("Failure" in statuses[nm])
-      { status="failure"; alt="unspecified"; num=orWhat(statuses[nm]["Failure"]) }
-    else if ("Unspecified" in statuses[nm])
-      { status="unspecified"; alt="success" }
-    else
-      { status="unspecified"; alt=true }
-    var tail = null
-    if (nm in chains) { tail = $("<sup>"); tail.text(chains[nm]) }
-    var nameMe = $('<span style="white-space:nowrap">')
-    var habitThing = habitDOM(nm, status, tail, setDone(day,nm,alt,0,update))
-    if (null !== num) { habitThing.addClass("pairLeft") }
-    nameMe.append(habitThing)
-    if (null !== num) {
-      var hack = $('<sub contenteditable="true">')
-      onEnter(hack,function(){ hack.blur() })
-      hack.blur(function(){
-        setDone(day,nm,status,parseFloat(this.textContent),update)()})
-      hack.text(num)
-      hack.addClass("refutable")
-      if (status) { hack.addClass(status) }
-      hack.addClass("pairRight")
-      nameMe.append(hack) }
-
-    habitList.append(nameMe)
-    habitList.append($("<span>"))
-    })
-
-  habitList.append(
-    $('<input type="text">')
-      .css("width","70")
-      .addClass("refutable")
-      .css("font-weight","bold")
-      .attr("placeholder", "addhabit")
-      .change(function(){addHabit(this.value, update)()}))
-
-  var node = $("#notices")
-  node.empty()
-  node.append(hdr)
-  node.append(habitList)
-  node.append($('<div id="noteListPlaceholder">'))
-  node.append($('<div id="historyPlaceholder">'))
-
-  var addNoteFn = function (name) { addNote(name, update)(); }
-  var renameFn = function (a,b) { delNote(a,addNote(b,update))() }
-  React.renderComponent(
-    <Notes addNote={addNoteFn} renameNote={renameFn} noteList={fsort(notes)} />,
-    document.getElementById("noteListPlaceholder"));
-
-  var d = _.map(fsort(_.keys(statuses)), function(habit,i){
-    return _.map(fsort(_.keys(history)), function(day,j){
-      return [day, habit, habitClass(history[day][habit])] })})
-
-  React.renderComponent(
-    <History habitData={d} />,
-    document.getElementById("historyPlaceholder")); }
+  // top.setState(state)
+  // React.unmountComponentAtNode(document.getElementById('notices'))
+  // React.renderComponent(
+    // <App state={state} />,
+    // document.getElementById("notices")) }
 
 
+//////////////////////////////////////////////////////////////////////////////
 // Inject CSS
 var stylesheet = "form {margin:0}"
 var node = document.createElement('style')
@@ -297,48 +366,32 @@ node.innerHTML = stylesheet
 document.body.appendChild(node)
 
 
+//////////////////////////////////////////////////////////////////////////////
 // Render the page.
-main = function() {
+var logout = null;
+var login = null;
+var top = null;
+main = function(){
   tok = localStorage.getItem("token")
   user = localStorage.getItem("username")
-  React.unmountComponentAtNode(document.getElementById('auth'));
-  $("#notices").empty()
-
-  var login = function (user,pass) {
+  React.unmountComponentAtNode(document.getElementById('auth'))
+  React.unmountComponentAtNode(document.getElementById('notices'))
+  login = function (user,pass) {
     var opts =
       { type:"POST", url:"/", dataType:"json", processData:false
       , data:toJSON({"Register":[user,pass]}) }
     $.ajax(opts).success(function(response) {
-      console.log(response);
+      // console.log(response)
       if ("AUTH" in response) {
         tok=response["AUTH"]
         localStorage.setItem("token",tok)
         localStorage.setItem("username",user)
-        console.log(user,pass,tok);
+        // console.log(user,pass,tok)
         main() }})}
-
-  var logout = function() {
+  logout = function(){
     var tok = localStorage.removeItem("token")
     var user = localStorage.removeItem("username")
     main() }
+  React.renderComponent(<App />, document.getElementById('notices')) }
 
-  var alreadyAuthenticated = (user && tok)
-
-  if (alreadyAuthenticated) {
-    console.log("hi!")
-    React.renderComponent(
-      <LogoutForm logout={logout} />,
-      document.getElementById('auth'))
-    update() }
-
-  else {
-    React.renderComponent(
-      <div>
-        Please login or register. If you try to
-        login with a username that doesn't exist,
-        the we will register you instead.
-        <LoginForm login={login}/>
-        </div>,
-      document.getElementById('auth')) }};
-
-main();
+main()
